4cd3d80c27d6b2c1cbd69ab3abfc1dc30c780ccc
chica25 <patricia@patriciaco.com>
2021-01-19 03:16:07 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,22 +1,25 @@
-let state = {friends: []} 
+let state = ( {
+    friends: []} )
 
-function manageFriends(state, action){
-        switch (action.type) {
+export function manageFriends(state, action){
+        switch(action.type) {
         
             case "ADD_FRIEND":
                  // console.log("action", action)
-            return { friends:[...state.friends, action.friends]}
+                return { friends:[...state.friends, action.friend]}
         
             // step 1 - find index of items to remove
             // step 2 - slice a new array from the old array excluding that index
+            
             case "REMOVE_FRIEND":
-                const removeId = state.friends.findIndex(friend => friend.id === action.id)
-                rn(
+                   const removeId = state.friends.findIndex(friend => friend.id === action.id)
+                
+                return(
                     {...state, friends:[...state.friends.slice(0, removeId),
                         ...state.friends.slice(removeId + 1) ]}
                 )
         
                 default:
                     return state;
-        }
+     }
 }
